6a432256141d3e7e5070de2b1b7a048ba17ddeda
Weasel On A Stick <korotkih.00@gmail.com>
2024-06-06 00:32:43 +0700

    optimized sample mixamo characters

---
+++ Assets/Samples/MapSelectorDemo/Scripts/MapSelector.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapSelector : ItemSelector<SelectableMap>
+{
+    [SerializeField]
+    TextMeshProUGUI _name_label;
+
+    [SerializeField]
+    TextMeshProUGUI _description_label;
+
+    [SerializeField]
+    Image _image_display;
+
+    public override void DisplayCurrentItem()
+    {
+        _name_label.text = Current_item.name;
+        _description_label.text = Current_item.description;
+        _image_display.sprite = Current_item.image;
+    }
+
+    public override void InitializeList()
+    {
+        // for variety let's switch between maps via "<" and ">" buttons
+    }
+}
--- Assets/Scripts/SelectableItem.cs
+++ Assets/Scripts/SelectableItem.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public class SelectableItem : ScriptableObject
 {
-
+    // In case all future items have shared properties (?)
 }
